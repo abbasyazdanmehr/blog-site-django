db315a54dca0297450ee076e6337d5fa6e50dc72
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2021-08-08 18:00:01 +0430

    customizatino of adminPanel and transform data from models

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,11 @@ from .models import Article
 
 # Register your models here.
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'published', 'status')
+    list_filter = ('published', 'status')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['status', 'published']
+
+admin.site.register(Article, ArticleAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,12 @@
+from blog.models import Article
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
 
 def home(request):
     context = {
-        "articles": [
-            {
-                "title": "رئیس کل بانک مرکزی در پاسخ به باشگاه خبرنگاران جوان:",
-                "description": "کمیجانی گفت به گلا دست نزنین.",
-                "image": "https://cdn.yjc.news/files/fa/news/1400/5/17/14481305_958.jpg"
-            },
-            {
-                "title": "رئیس شورای شهر تهران:",
-                "description": "چمران گفت به گلا دست نزنین.",
-                "image": "https://cdn.yjc.news/files/fa/news/1400/5/17/14480399_203.jpg"
-            },
-            {
-                "title": "ازار خودرو زیر ذره بین باشگاه خبرنگاران جوان؛",
-                "description": "قیمت خودرو، امروز در بازار آزاد نسبت به آخرین معاملات دیروز با افزایش ",
-                "image": "https://cdn.yjc.news/files/fa/news/1400/5/17/14481136_464.jpg"
-            }
-        ]
+        "articles": Article.objects.filter(status='p').order_by('published')
     }
     return render(request, "blog/home.html", context)
 
