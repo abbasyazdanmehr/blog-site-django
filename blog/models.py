0edc0e778580d211cea3ce4f758320fec86452d0
abbas yazdanmehr <77411860+abbasyazdanmehr@users.noreply.github.com>
2021-08-09 16:20:27 +0430

    فارسی سازی سایت ادمین :alien:

--- blog/models.py
+++ blog/models.py
@@ -10,14 +10,20 @@ class Article(models.Model):
         ('p', 'Published'),
     )
     
-    title = models.CharField(max_length=30)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    published = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=30, verbose_name='عنوان')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='شناسه')
+    description = models.TextField(verbose_name='توضیحات')
+    thumbnail = models.ImageField(upload_to="images", verbose_name='تصویر')
+    published = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقاله ها"
+    
     def __str__(self):
         return self.title
+    
+    
